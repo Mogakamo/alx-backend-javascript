2c46fb036286ebf424dbce73a13b27d4a7d3e682
Mogakamo <mogaka.amo254@gmail.com>
2022-06-28 15:41:09 +0300

    6 error solved

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,21 @@
-import signUpUser from './4-all-reject';
-import uploadPhoto from './5-all-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
+export default async function handleProfileSignUp(firstName, lastName, fileName) {
+  const stats = [];
+  await signUpUser(firstName, lastName)
+    .then(async (data) => {
+      stats.push({
+        status: 'fulfilled',
+        value: data,
+      });
+      await uploadPhoto(fileName);
+    })
+    .catch((err) => {
+      stats.push({
+        status: 'rejected',
+        value: err.toString(),
+      });
     });
-  }
-  return res;
+  return stats;
 }
