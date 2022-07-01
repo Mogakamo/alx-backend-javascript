1f6a60b3d05d8e726cab6b65b1e6e79e80150d1d
Mogakamo <mogaka.amo254@gmail.com>
2022-07-01 04:48:46 +0300

    commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,5 @@
 import ClassRoom from "./0-classrooms";
 
 export default function initializeRooms () {
-  new ClassRoom(19);
-  new ClassRoom(20);
-  new ClassRoom(34);
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
